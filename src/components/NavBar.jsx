6759ceb73ce97893036dca1b729a122e635e44e5
NicoSciangula <nico.sciangula99@gmail.com>
2026-02-08 20:12:46 +0100

    fixed: added creation of elements with map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,56 @@
+const barData = [
+  {
+    id: 1,
+    name: "CHARACTERS",
+  },
+  {
+    id: 2,
+    name: "COMICS",
+  },
+  {
+    id: 3,
+    name: "MOVIES",
+  },
+  {
+    id: 4,
+    name: "TV",
+  },
+  {
+    id: 5,
+    name: "GAMES",
+  },
+  {
+    id: 6,
+    name: "COLLECTIBILES",
+  },
+  {
+    id: 7,
+    name: "VIDEOS",
+  },
+  {
+    id: 8,
+    name: "FANS",
+  },
+  {
+    id: 9,
+    name: "NEWS",
+  },
+  {
+    id: 10,
+    name: "SHOP",
+  },
+];
+
 export default function NavBar() {
-    return (
-        <>
-            <ul className="nav-bar">
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAMES</a></li>
-                <li><a href="#">COLLECTIBILES</a></li>
-                <li><a href="#">VIDEOS</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
-            </ul>
-        </>
-    )
+  return (
+    <>
+      <ul className="nav-bar">
+        {barData.map((data) => (
+          <li key={data.id}>
+            <a href="#">{data.name}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
