081e178be0885c1b7dbfcdd176cd8aad7119a65a
NicoSciangula <nico.sciangula99@gmail.com>
2026-02-08 17:09:24 +0100

    added list navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+export default function NavBar() {
+    return (
+        <>
+            <ul className="nav-bar">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBILES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </>
+    )
+}
