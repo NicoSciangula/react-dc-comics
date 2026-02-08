ee07eb7248e19eafc8c0c74d67797514b21d3074
NicoSciangula <nico.sciangula99@gmail.com>
2026-02-08 19:57:55 +0100

    imported CustomerBar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import ProductList from "./product/ProductList";
+import CustomerBar from "./CustomerBar";
 
 export default function Main() {
   return (
     <>
       <main>
         <ProductList />
+        <CustomerBar />
       </main>
     </>
   );
