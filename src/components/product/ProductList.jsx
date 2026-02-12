bc1b9dca7a59630822265982053e0dfe559f2ead
NicoSciangula <nico.sciangula99@gmail.com>
2026-02-12 21:48:19 +0100

    imported child component

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,4 +1,5 @@
 import comics from "../../../dc-comics-2/dc-comics-2/comics";
+import ProductCard from "./ProductCard";
 
 export default function ProductList() {
   return (
@@ -6,12 +7,7 @@ export default function ProductList() {
       <section id="product">
         <span className="positioning">CURRENT SERIES</span>
         <div className="container-wrap">
-          {comics.map((comic) => (
-            <article key={comic.id} className="product-card">
-              <img src={comic.thumb} alt={comic.description} />
-              <p>{comic.series.toUpperCase()}</p>
-            </article>
-          ))}
+          <ProductCard items={comics}/>
         </div>
         <button className="btn">LOAD MORE</button>
       </section>
